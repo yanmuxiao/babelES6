b0d8fdc614b8a9046bb51bdb04ce6f115a05a8d6
yanmuxiao <412559950@qq.com>
2018-12-29 15:43:05 +0800

    index.js _.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+function log(clog) {
+	console.log(clog);
+}
+
 var aObj = {a: 'a'};
 var bObj = {b: 'b'};
 var arr = [aObj, bObj, undefined, null, aObj, bObj, '', ' ','1', 2, undefined, null, 'a', 'b', 1, '2', 2, 'c', 1, 2, 'NaN' ,NaN, NaN, new Date(), [1], [1,2], {a: 'a'}, {b:'b'}];
+
 // 利用indexOf
-function indexOfUniq(arr) {
-	var len = arr.length;
+function uniq(arr) {
 	var uniqResult = [];
 	var hasNaN = false;
-	for(var i = 0; i < len; i++) {
+	for(var i = 0, len = arr.length; i < len; i++) {
 		if(arr[i] !== arr[i]) {
 			if(!hasNaN) {
 				hasNaN = true;
@@ -18,55 +22,292 @@ function indexOfUniq(arr) {
 	}
 	return uniqResult;
 }
-// console.log(indexOfUniq(arr));
-// 利用Object的属性判断
-// 缺陷：不能判断引用类型，不能区分NaN和'NaN'
-Array.prototype.unique = function () {
-    var res = [];
-    var json = {};
-    for (var i = 0; i < this.length; i++) {
-        if (!json[this[i]]) {
-            res.push(this[i]);
-            json[this[i]] = 1;
-        }
-    }
-    return res;
-}
-// console.log(arr.unique());
-
+// log(uniq(arr));
 
 // 利用indexOf
 // 通过数组对象的某个key值判断是否唯一，key值最好为数字或字符串
-var arrObj = [{id: 1, name: 'a'}, {id: '1', name: 'b'}, {id: 'c', name: 'c'}, {id: 'a', title: 'a'}, {id: 'b', title: 'b'}, {id: 'c', title: 'c'}];
-function arrObjUniqBy(arrObj, key) {
-	var len = arrObj.length;
+var arrObj = [{id: NaN, name: 'NaN'},{id: NaN, name: 'a'},{id: 1, name: 'a'}, {id: '1', name: 'b'}, {id: 'c', name: 'c'}, {id: 'a', title: 'a'}, {id: 'b', title: 'b'}, {id: 'c', title: 'c'}];
+function uniqBy(arrObj, key) {
 	var uniqResult = [];
 	var keyArr = [];
-	for(var i = 0; i < len; i++) {
-		if(keyArr.indexOf(arrObj[i][key]) === -1 && arrObj[i][key]) {
-			keyArr.push(arrObj[i][key]);
-			uniqResult.push(arrObj[i]);
+	var hasNaN = false;
+	for(var i = 0, len = arrObj.length; i < len; i++) {
+		if(arrObj[i][key] !== undefined && keyArr.indexOf(arrObj[i][key]) === -1) {
+			if(arrObj[i][key] !== arrObj[i][key]) {
+				if(!hasNaN) {
+					hasNaN = true;
+					uniqResult.push(arrObj[i]);
+				}
+			}else{
+				keyArr.push(arrObj[i][key]);
+				uniqResult.push(arrObj[i]);
+			}
 		}
 	}
 	return uniqResult;
 }
-console.log(arrObjUniqBy(arrObj, 'id'));
+// log(uniqBy(arrObj, 'id'));
 
 
 
-var objects = [{ 'x': 1.2, 'y': 2 }, { 'x': 2.3, 'y': 1 }, { 'x': 1.8, 'y': 2 }, { 'x': 0.8, 'y': 1 }];
-function arrObjUniqWith(arrObj, fn) {
+var objects = [{ 'x': 1.2, 'y': 2 }, { 'x': 2.3, 'y': 1 }, { 'x': 1.8, 'y': 2 }, { 'x': 0.8, 'y': 1 }, { 'x': 1.1, 'y': 2 }];
+function uniqWith(arrObj, fn) {
 	var len = arrObj.length;
 	var uniqResult = [];
 	for(var i = 0; i < len; i++) {
-		if(fn(arrObj[i][key], uniqResult)) {
+		var uniqBoolean = true;
+		for(var j = 0; j < uniqResult.length; j++) {
+			if(fn(arrObj[i], uniqResult[j])) {
+				uniqBoolean = false;
+				break;
+			}
+		}
+		if(uniqBoolean) {
 			uniqResult.push(arrObj[i]);
 		}
 	}
 	return uniqResult;
 }
-arrObjUniqWith(objects, function(o){
-	return Math.floor()
-});
+var uniqWithResult = uniqWith(objects, mathFloor);
+function mathFloor(o1, o2) {
+	return Math.floor(o1.x) === Math.floor(o2.x);
+}
+// log(uniqWithResult);
 
 
+function difference(arr1, arr2) {
+	var diffResult = [];
+	for(var i = 0, len1 = arr1.length; i < len1; i++) {
+		var diffBoolean = true;
+		for(var j = 0, len2 = arr2.length; j < len2; j++) {
+			if((arr1[i] !== arr1[i] && arr2[j] !== arr2[j]) || arr1[i] === arr2[j]) {
+				diffBoolean = false;
+				break;
+			}
+		}
+		if(diffBoolean) {
+			diffResult.push(arr1[i]);
+		}
+	}
+	return diffResult;
+}
+var a = {}, b = {};
+var arr1 = [1, {a: 'a'}, a, b,[], [], 'a', 'b', '1', null, undefined, false, true, NaN, NaN, NaN]; 
+var arr2 = [null, NaN, 'a', '1', a, b, undefined, false];
+// log(difference(arr1, arr2));
+
+
+function differenceBy(arrObj1, arrObj2, key) {
+	var diffResult = [];
+	var arrObj2Keys = [];
+	for(var j = 0, len2 = arrObj2.length; j < len2; j++) {
+		arrObj2Keys.push(arrObj2[j][key]);
+	}
+	for(var i = 0, len1 = arrObj1.length; i < len1; i++) {
+		if(arrObj2Keys.indexOf(arrObj1[i][key]) == -1) {
+			diffResult.push(arrObj1[i]);
+		}
+	}
+	return diffResult;
+}
+var diffByArr1 = [{id: '1', name: 'Tom'}, {id: '2', name: 'Hailey'}, {id: '3', name: 'Bright'}];
+var diffByArr2 = [{id: '1', name: '1'}, {id: '1', name: 'Tom'}, {id: '5', name: '123456'}];
+// log(differenceBy(diffByArr1, diffByArr2, 'id'));
+// log(differenceBy(diffByArr1, diffByArr2, 'name'));
+
+
+var diffWithArr1 = [{id: '1.2', name: 'Tom'}, {id: '2.1', name: 'Hailey'}, {id: '3.9', name: 'Bright'}];
+var diffWithArr2 = [{id: '3.9', name: 'Bright'}, {id: '1.2', name: 'Tom'}, {id: '5', name: '123456'}];
+function differenceWith(arrObj1, arrObj2, fn) {
+	var diffResult = [];
+	for(var i = 0, len1 = arrObj1.length; i < len1; i++) {
+		var diffBoolean = true;
+		for(var j = 0, len2 = arrObj2.length; j < len2; j++) {
+			if(fn(arrObj1[i], arrObj2[j])) {
+				diffBoolean = false;
+				break;
+			}
+		}
+		if(diffBoolean) {
+			diffResult.push(arrObj1[i]);
+		}
+	}
+	return diffResult;
+}
+function mathFloorId(o1, o2) {
+	return Math.floor(o1.id) === Math.floor(o2.id);
+}
+function startWith(o1, o2) {
+	return (/^Tom/i.test(o1.name)&&/^Tom/i.test(o2.name));
+}
+// log(differenceWith(diffWithArr1, diffWithArr2, mathFloorId));
+// log(differenceWith(diffWithArr1, diffWithArr2, startWith));
+// log(differenceWith(diffWithArr1, diffWithArr2, isEqual));
+
+
+
+// 复杂类型只考虑Boolean, Number, String, Object, Array
+// new Boolean()的返回值是一个对象，Boolean(new Boolean())这个为真
+// 判断new Boolean(bool)的真假 ==> new Boolean(bool).toString() ==> 转化为字符串类型
+
+
+// 判断Object的缺陷：
+// 1、key==>value中的value不能是NaN，也就是不能区分null和NaN
+// 2、key==>value中的value不能是undefined，否则不比较当前的键值
+function isEqual(e1, e2) {
+	var typeofE1 = typeof e1;
+	var typeofE2 = typeof e2;
+	if(e1 === e2) {
+		log('===');
+		return true;
+	}else{
+		if(e1 !== e1 || e2 !== e2){
+			log('NaN:');
+			return (e1 !== e1 && e2 !== e2);
+		}else if(e1 === null || e2 === null || typeofE1 === 'function' || typeofE2 === 'function'){
+			log('null || function:');
+			return false;
+		}else if(typeofE1 === 'object' && typeofE2 === 'object') { // 不存在null, NaN, function，并且两个都是复杂类型
+			if(e1 instanceof Array && e2 instanceof Array){
+				if(e1.length !== e2.length || JSON.stringify(e1) !== JSON.stringify(e2)) { // 粗略判断检验
+					log('粗略判断检验:');
+					return false;
+				}else{
+					var arrEqual = true;
+					for(var i = 0, len = e1.length; i < len; i++) {
+						if(!isEqual(e1[i], e2[i])) {
+							arrEqual = false;
+							break;
+						}
+					}
+					log('Array:');
+					return arrEqual;
+				}
+			}else if(e1.constructor === Object && e2.constructor === Object) {
+				log('Object判断：（不能辨别null和NaN）');
+				return JSON.stringify(e1) === JSON.stringify(e2);
+			}else if(e1 instanceof Number && e2 instanceof Number) {
+				log('Number:');
+				return Number(e1) === Number(e2);
+			}else if(e1 instanceof String && e2 instanceof String) {
+				log('String:');
+				return String(e1) === String(e2);
+			}else if(e1 instanceof Boolean && e2 instanceof Boolean) {
+				log('Boolean:');
+				return e1.toString() === e2.toString();
+			}else{
+				log('非数组、Object、Number、String、Boolean的判断:');
+				return false;
+			}
+		}else{ // 不存在null, NaN, function，并且一个是简单类型，一个是复杂类型
+			var e1isObj = typeof e1 === 'object';
+			var e2isObj = typeof e2 === 'object';
+			var E1, E2;
+			if(e1 instanceof Number || e2 instanceof Number) {
+				log('Number:');
+				E1 = e1isObj ? Number(e1) : e1;
+				E2 = e2isObj ? Number(e2) : e2;
+				return E1 === E2;
+			}else if(e1 instanceof String || e2 instanceof String) {
+				log('String:');
+				E1 = e1isObj ? String(e1) : e1;
+				E2 = e2isObj ? String(e2) : e2;
+				return E1 === E2;
+			}else if(e1 instanceof Boolean) {
+				log('e1 is Boolean Object:');
+				return (typeof e2 === 'boolean') && e1.toString() === e2.toString();
+			}else if(e2 instanceof Boolean) {
+				log('e2 is Boolean Object:');
+				return (typeof e1 === 'boolean') && e1.toString() === e2.toString();
+			}else{
+				log('简单类型和复杂类型的比较:');
+				return false;
+			}
+		}
+	}
+}
+
+// log(isEqual(new Boolean(), new Boolean(2)));
+// log(isEqual({a: undefined}, {a: NaN}));
+// log(isEqual([null, undefined, NaN, false, new Boolean(), true, new Boolean(1)], [null, undefined, NaN, false, new Boolean(), true, new Boolean(1)]));
+// log(isEqual([{a: null}, {b: +0}], [{a: NaN}, {b: -0}]));
+// log(isEqual([{a: null}, {b: +0}], [{a: NaN}, {b: -0}]));
+
+function forEach(arr, fn) {
+	for(var i = 0, len = arr.length; i < len; i++) {
+		fn(arr[i], i, arr);
+	}
+}
+function find(arr, fn) {
+	var fValue = false;
+	for(var i = 0, len = arr.length; i < len; i++) {
+		if(fn(arr[i], i, arr)) {
+			fValue = arr[i];
+			break;
+		}
+	}
+	return fValue;
+}
+function findIndex(arr, fn) {
+	var fIndex = false;
+	for(var i = 0, len = arr.length; i < len; i++) {
+		if(fn(arr[i], i, arr)) {
+			fIndex = i;
+			break;
+		}
+	}
+	return fIndex;
+}
+function filter(arr, fn) {
+	var filterResult = [];
+	for(var i = 0, len = arr.length; i < len; i++) {
+		if(fn(arr[i], i, arr)) {
+			filterResult.push(arr[i]);
+		}
+	}
+	return filterResult;
+}
+var some = function(arr, fn) {
+	var someTrue = false;
+	for(var i = 0, len = arr.length; i < len; i++) {
+		if(fn(arr[i], i, arr)) {
+			someTrue = true;
+			break;
+		}
+	}
+	return someTrue;
+}
+var every = function(arr, fn) {
+	var everyTrue = true;
+	for(var i = 0, len = arr.length; i < len; i++) {
+		if(!fn(arr[i], i, arr)) {
+			everyTrue = false;
+			break;
+		}
+	}
+	return everyTrue;
+}
+var map = function(arr, fn) {
+	var mapResult = [];
+	for(var i = 0, len = arr.length; i < len; i++) {
+		mapResult.push(fn(arr[i], i, arr));
+	}
+	return mapResult;
+}
+
+var ages = [3, 10, 18, 20];
+function checkAdult(age) {
+    return age >= 2 && age < 20;
+}
+ 
+// forEach(ages, function(v, i) {
+// 	log(i + '==>' + v);
+// })
+// log(find(ages, checkAdult));
+// log(findIndex(ages, checkAdult));
+// log(filter(ages, checkAdult));
+// log(some(ages, checkAdult));
+// log(every(ages, checkAdult));
+log(map(ages, function(v, i) {
+	return v * i;
+}))
